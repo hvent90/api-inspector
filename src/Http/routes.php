5f28cc166a012645a7f653a598c825816d0dee9d
Henry Ventura <hvent90@gmail.com>
2015-04-28 23:05:25 -0400

    fixed routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,7 +1,8 @@
 <?php
 $config = $this->app['config']->get('api-inspector');
+$routeModifiers = $config['route-modifiers'];
 
-Route::group($config['route-modifiers'], function() {
+Route::group($routeModifiers ? $routeModifiers : [], function() {
 	$config = $this->app['config']->get('api-inspector');
 
 	Route::get($config['uri'], 'ApiInspectorController@stream');
